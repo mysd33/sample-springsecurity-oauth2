cc533960c669b6de021c989546f80ed447a43838
mysd33 <mysd.33@gmail.com>
2023-01-23 16:24:04 +0900

    各種トークン情報を取得するサンプルコード追加

--- src/main/java/com/example/bff/app/LoginController.java
+++ src/main/java/com/example/bff/app/LoginController.java
@@ -1,7 +1,17 @@
 package com.example.bff.app;
 
+import java.time.Instant;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.OAuth2RefreshToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,17 +20,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 
  * ログイン機能のコントローラクラス
  *
  */
+@Slf4j
 @Controller
 public class LoginController {
 
+    @Autowired
+    OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+
     @ModelAttribute
     public LoginForm setUpForm() {
-        return new LoginForm();        
+        return new LoginForm();
     }
 
     /**
@@ -49,10 +65,49 @@ public class LoginController {
      * ログイン成功後のメニュー画面遷移処理
      */
     @GetMapping("/menu")
-    public String menu(Model model) {
+    // ハンドラメソッドの引数でOIDC、OAuth関連データを取得する例
+    public String menu(@AuthenticationPrincipal OidcUser oidcUser, OAuth2AuthenticationToken oAuth2AuthenticationToken,
+            Model model) {
+//　 public String menu(Model model) {
+        if (oidcUser != null) {
+            // IDトークンの取得
+            String idToken = oidcUser.getIdToken().getTokenValue();
+            log.debug("[IdToken]:{}", idToken);
+            // IDトークンの属性の取得
+            oidcUser.getAttributes().forEach((k, v) -> log.debug("[{}]:{}", k, v));
+        }
+        if (oAuth2AuthenticationToken != null) {
+            String registrationId = oAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
+            String principalName = oAuth2AuthenticationToken.getName();
+
+            OAuth2AuthorizedClient oAuth2AuthorizedClient = oAuth2AuthorizedClientService
+                    .loadAuthorizedClient(registrationId, principalName);
+            if (oAuth2AuthorizedClient != null) {
+                // アクセストークンの取得
+                OAuth2AccessToken accessToken = oAuth2AuthorizedClient.getAccessToken();
+                String accessTokenValue = accessToken.getTokenValue();
+                Instant accessTokenIssueAt = accessToken.getIssuedAt();
+                Instant accessTokenExpiresAt = accessToken.getExpiresAt();
+                log.debug("[AccessToken]:{}", accessTokenValue);
+                log.debug("[AccessToken ValidPeriod]from {} to {}", accessTokenIssueAt, accessTokenExpiresAt);
+                // リフレッシュトークンの取得
+                OAuth2RefreshToken refreshToken = oAuth2AuthorizedClient.getRefreshToken();
+                if (refreshToken != null) {
+                    String refreshTokenValue = refreshToken.getTokenValue();
+                    Instant refreshTokenIssueAt = refreshToken.getIssuedAt();
+                    Instant refreshTokenExpiresAt = refreshToken.getExpiresAt();
+                    log.debug("[RefreshToken]:{}", refreshTokenValue);
+                    log.debug("[RefreshToken ValidPeriod]from {} to {}", refreshTokenIssueAt, refreshTokenExpiresAt);
+                }
+
+            }
+
+        }
+
         return "menu/menu";
+
     }
-    
+
     /**
      * 
      * OAuthログイン画面のページ遷移処理
@@ -67,9 +122,8 @@ public class LoginController {
      * 管理者用ユーザ管理ページ遷移用処理
      */
     /*
-    @GetMapping("/admin")
-    public String admin(Model model) {
-        return "redirect:/userList";
-    }*/
+     * @GetMapping("/admin") public String admin(Model model) { return
+     * "redirect:/userList"; }
+     */
 
 }
